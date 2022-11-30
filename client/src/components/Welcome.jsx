69f46de1850d00cb40fe26c8ef5d6cd56ef79dc4
delmond147 <delmondbongha147@gmail.com>
2022-11-29 23:43:12 -0800

    initial commit

---
+++ client/src/components/Welcome.jsx
@@ -0,0 +1,17 @@
+import {AiFillPlayCircle} from 'react-icons/ai'
+import {SiEthereum} from 'react-icons/si'
+import {BsInfoCircle} from 'react-icons/bs'
+
+import {Loader} from './'
+
+const Welcome = () => {
+    return (
+        <div className='flex w-full justify-center items-center'>
+            <div>
+            
+            </div>
+        </div>
+    )
+}
+
+export default Welcome
